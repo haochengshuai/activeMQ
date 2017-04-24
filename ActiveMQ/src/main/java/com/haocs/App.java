7e3005070c5d85eafc1a7f941f6fbd6153229734
hcs <hcs@172.27.11.9>
2017-04-24 10:33:51 +0800

    init

---
+++ ActiveMQ/src/main/java/com/haocs/App.java
@@ -0,0 +1,13 @@
+package com.haocs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
